87f6f6ab57d733893e6d5e48e6b2c2b8de1e822a
Amy Speed <amy.speed.henley@gmail.com>
2018-11-28 20:35:58 -0600

    recipes Post script

--- server.js
+++ server.js
@@ -47,10 +47,25 @@ app.post('/shopping-list', jsonParser, (req, res) => {
   res.status(201).json(item);
 });
 
-
 app.get('/recipes', (req, res) => {
   res.json(Recipes.get());
 })
+
+app.post('/recipes', jsonParser, (req, res)=> {
+  const requiredEntry = ['name', 'ingredients'];
+  for (let i=0; i<requiredEntry.length; i++) {
+    const entry = requiredEntry[i];
+    if (!(entry in req.body)) {
+      const message = `Missing \`${entry}\` in request body`
+      console.error(message);
+      return res.status(400).send(message);
+    }
+  }
+
+  const recipeItem = Recipes.create(req.body.name, req.body.ingredients);
+  res.status(201).json(recipeItem)
+});
+
 
 app.listen(process.env.PORT || 8080, () => {
   console.log(`Your app is listening on port ${process.env.PORT || 8080}`);
